2c52917d328022141ca5eea3f1cd0cdbb1c9b1a9
Aniekan Winner Anietie <winneraaniekan@gmail.com>
2024-12-19 03:54:28 +0100

    Update swagger.js to add Authentication documentation

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -8,11 +8,32 @@ const options = {
 			title: "Product Management API",
 			version: "1.0.0",
 			description:
-				"API Documentation for my e-commerce product management system",
+				"API Documentation for my e-commerce product management system with GitHub OAuth authentication",
 		},
 		servers: [
 			{
-				url: "https://product-api-d8ak.onrender.com/",
+				url: "http://localhost:3000/",
+			},
+		],
+		components: {
+			securitySchemes: {
+				githubAuth: {
+					type: "oauth2",
+					flows: {
+						authorizationCode: {
+							authorizationUrl: "https://github.com/login/oauth/authorize",
+							tokenUrl: "https://github.com/login/oauth/access_token",
+							scopes: {
+								"read:user": "Read user information",
+							},
+						},
+					},
+				},
+			},
+		},
+		security: [
+			{
+				githubAuth: [],
 			},
 		],
 	},
